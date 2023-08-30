a782a59ced9a5266ca401f5bcd7ab828e549fd63
djerada-madjid <djerada.madjid@gmail.com>
2023-08-30 15:26:02 +0200

    Refresh UI with new messages

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,7 @@
-async function getMessages(token) {
-    return await fetch('http://localhost:3000/messages', {
+
+async function getMessages(token, dateAfterTs) {
+    const filter = (dateAfterTs) ? "?dateAfterTs=" + dateAfterTs : ""
+    return await fetch(`http://localhost:3000/messages${filter}`, {
         method: "GET",
         //mode: 'same-origin',
         headers: {
@@ -8,6 +10,17 @@ async function getMessages(token) {
     })
 
 };
+
+const latMessagtTimestamp = () => {
+    let ts = 0;
+    return {
+        get: () => ts,
+        set: (time) => { ts = time }
+    }
+}
+
+
+const tsManager = latMessagtTimestamp();
 
 async function getMsgsOrRedirect() {
     const token = localStorage.getItem("token");
@@ -24,21 +37,34 @@ async function getMsgsOrRedirect() {
 }
 
 
-getMsgsOrRedirect().then((messages) => {
-    if(!messages) return;
-    const infoToDisp = messages.map( e =>{
-        const {login, content, date : ts} =e;
+const displayMsgs = (messages) => {
+    if (!messages) return;
+    const infoToDisp = messages.map((e, i) => {
+        const { login, content, date: ts } = e;
         const date = new Date(ts);
+        if (i === messages.length - 1) {
+            tsManager.set(ts);
+        }
         return {
             login,
             content,
-            date : `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()} `
+            date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()} `
         }
     });
-    
-    infoToDisp.forEach((e) => {
+
+    infoToDisp.forEach((e, i) => {
         document.getElementById("chatBox").appendChild(createSpan(e));
     });
+}
+
+
+getMsgsOrRedirect().then((messages) => {
+    displayMsgs(messages);
+    setInterval(async () => {
+        const token = localStorage.getItem("token");
+        const messages = await getMessages(token, tsManager.get());
+        displayMsgs(await messages.json());
+    }, 1000)
 });
 
 function createSpan(elem) {
@@ -53,15 +79,16 @@ async function saveMessages() {
     const msg = document.getElementById("valueMessage").value;
     if (!msg) return;
     const token = localStorage.getItem("token");
-    return await fetch ('http://localhost:3000/messages/send',{
+    return await fetch('http://localhost:3000/messages/send', {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             Authorization: token,
         },
-        body: JSON.stringify({msg})
+        body: JSON.stringify({ msg })
     })
 };
+
 
 
 
